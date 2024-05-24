b1fea650c47aa066586938599137d49f7a6e65e1
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-24 18:46:57 +0200

    created migration and pushed on database

---
+++ database/migrations/2024_05_24_164544_create_events_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',200);
+            $table->string('city',100);
+            $table->string('address',200);
+            $table->decimal('ticket_price', 5,2)->unsigned();
+            $table->date('date_start_event');
+            $table->date('date_end_event');
+            $table->boolean('open_reservation');
+            $table->string('location_url', 10000);
+            $table->text('overview');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
