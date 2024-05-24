87ec43b8d0678feddc9a7b8ff472ab08f0d13a95
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-24 20:29:42 +0200

    created footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,27 @@
-<div>footer</div>
+<footer class="container py-5">
+    <form>
+        <h5>Subscribe to our newsletter</h5>
+        <p>Monthly digest of what's new and exciting from us.</p>
+        <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+            <label for="newsletter1" class="visually-hidden">Email address</label>
+            <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+            <button class="btn btn-primary" type="button">Subscribe</button>
+        </div>
+    </form>
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3 pt-5">
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
+    </ul>
+    <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4">
+        <span>&copy; 2024 Pr Quadro Events Inc. All rights reserved.</span>
+        <ul class="list-unstyled d-flex">
+            <li class="mx-2 fs-2"><a href="#twitter"><i class="fa-brands fa-instagram"></i></a></li>
+            <li class="mx-2 fs-2"><a href="#twitter"><i class="fa-brands fa-x-twitter"></i></a></li>
+            <li class="mx-2 fs-2"><a href="#twitter"><i class="fa-brands fa-youtube"></i></a></li>
+            <li class="mx-2 fs-2"><a href="#twitter"><i class="fa-brands fa-facebook"></i></a></li>
+        </ul>
+    </div>
+</footer>
